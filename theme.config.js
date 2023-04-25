3fc2218e947567ee69f381fcee803aaf6e073188
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-04-25 18:10:10 +0530

    Fix: minor typo

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,6 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Siddharth Tewari.
       <style jsx>{`
         a {
           float: right;
